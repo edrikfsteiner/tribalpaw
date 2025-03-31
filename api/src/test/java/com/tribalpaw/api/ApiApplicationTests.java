1f03d03783d1f1c5780d6d6cede22ddc338390d1
edrikfsteiner <edrikfsteiner@gmail.com>
2025-03-31 20:28:38 -0300

    First commit

---
+++ api/src/test/java/com/tribalpaw/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tribalpaw.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
