2f877e942e3b655b1d5be0b2c1971c6033b78549
edrikfsteiner <edrikfsteiner@gmail.com>
2025-04-11 19:56:55 -0300

    Added services

--- api/src/main/java/com/tribalpaw/api/service/CountryService.java
+++ api/src/main/java/com/tribalpaw/api/service/CountryService.java
@@ -1,4 +1,62 @@
 package com.tribalpaw.api.service;
 
+import com.tribalpaw.api.dto.country.CountryRequestDto;
+import com.tribalpaw.api.dto.country.CountryResponseDto;
+import com.tribalpaw.api.exception.BadRequestException;
+import com.tribalpaw.api.exception.NotFoundException;
+import com.tribalpaw.api.mapper.CountryMapper;
+import com.tribalpaw.api.model.country.Country;
+import com.tribalpaw.api.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CountryService {
+    @Autowired
+    private CountryRepository repository;
+
+    public List<CountryResponseDto> getAll() {
+        List<Country> countries = repository.findAll();
+        if (countries.isEmpty()) {
+            throw new NotFoundException("No countries found");
+        }
+        return countries.stream().map(CountryMapper::toDto).toList();
+    }
+
+    public CountryResponseDto getById(Long id) {
+        return CountryMapper.toDto(
+                repository.findById(id)
+                        .orElseThrow(() -> new NotFoundException("Country not found"))
+        );
+    }
+
+    @Transactional
+    public CountryResponseDto post(CountryRequestDto dto) {
+        return CountryMapper.toDto(repository.save(CountryMapper.toModel(dto)));
+    }
+
+    @Transactional
+    public CountryResponseDto put(CountryRequestDto dto) {
+        Optional<Country> country = repository.findById(dto.id());
+
+        if (country.isEmpty()) {
+            throw new NotFoundException("Army not found");
+        }
+        if (dto.name().isEmpty()) {
+            throw new BadRequestException("Invalid name");
+        }
+
+        return CountryMapper.toDto(repository.save(CountryMapper.toModel(dto)));
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Country not found"));
+        repository.deleteById(id);
+    }
 }
--- api/src/main/java/com/tribalpaw/api/service/ProvinceService.java
+++ api/src/main/java/com/tribalpaw/api/service/ProvinceService.java
@@ -1,4 +1,66 @@
 package com.tribalpaw.api.service;
 
+import com.tribalpaw.api.dto.province.ProvinceRequestDto;
+import com.tribalpaw.api.dto.province.ProvinceResponseDto;
+import com.tribalpaw.api.exception.BadRequestException;
+import com.tribalpaw.api.exception.NotFoundException;
+import com.tribalpaw.api.mapper.ProvinceMapper;
+import com.tribalpaw.api.model.province.Province;
+import com.tribalpaw.api.repository.ProvinceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProvinceService {
+    @Autowired
+    private ProvinceRepository repository;
+
+    public List<ProvinceResponseDto> getAll() {
+        List<Province> provinces = repository.findAll();
+        if (provinces.isEmpty()) {
+            throw new NotFoundException("No provinces found");
+        }
+        return provinces.stream().map(ProvinceMapper::toDto).toList();
+    }
+
+    public ProvinceResponseDto getById(Long id) {
+        return ProvinceMapper.toDto(
+                repository.findById(id)
+                        .orElseThrow(() -> new NotFoundException("Province not found"))
+        );
+    }
+
+    @Transactional
+    public ProvinceResponseDto post(ProvinceRequestDto dto) {
+        return ProvinceMapper.toDto(repository.save(ProvinceMapper.toModel(dto)));
+    }
+
+    @Transactional
+    public ProvinceResponseDto put(ProvinceRequestDto dto) {
+        Optional<Province> province = repository.findById(dto.id());
+        Optional<Province> country = repository.findById(dto.country().getId());
+
+        if (province.isEmpty()) {
+            throw new NotFoundException("Province not found");
+        }
+        if (dto.name().isEmpty()) {
+            throw new BadRequestException("Invalid name");
+        }
+        if (country.isEmpty()) {
+            throw new BadRequestException("Invalid country");
+        }
+
+        return ProvinceMapper.toDto(repository.save(ProvinceMapper.toModel(dto)));
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Province not found"));
+        repository.deleteById(id);
+    }
 }
