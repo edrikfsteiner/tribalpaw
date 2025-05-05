a19ed4cb687d29a97e527ebe2f69c47a20237bba
edrikfsteiner <edrikfsteiner@gmail.com>
2025-05-05 20:31:31 -0300

    Added test army creator

--- api/src/test/java/com/tribalpaw/api/util/ArmyCreator.java
+++ api/src/test/java/com/tribalpaw/api/util/ArmyCreator.java
@@ -6,15 +6,15 @@ import com.tribalpaw.api.model.army.Army;
 import com.tribalpaw.api.model.country.Country;
 import com.tribalpaw.api.model.division.Division;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ArmyCreator {
     private static final Country country = CountryCreator.createCountry();
-    private static final ArrayList<Division> divisions = DivisionCreator.createDivisions();
+    private static final List<Division> divisions = DivisionCreator.createDivisions();
     private static final Country countryRequestDto = CountryCreator.createCountryRequestDto();
-    private static final ArrayList<Division> divisionsRequestDto = DivisionCreator.createDivisionsRequestDto();
+    private static final List<Division> divisionsRequestDto = DivisionCreator.createDivisionsRequestDto();
     private static final Country countryResponseDto = CountryCreator.createCountryResponseDto();
-    private static final ArrayList<Division> divisionsResponseDto = DivisionCreator.createDivisionsResponseDto();
+    private static final List<Division> divisionsResponseDto = DivisionCreator.createDivisionsResponseDto();
 
     public static Army createArmy() {
         return Army.builder()
