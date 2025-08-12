1f026eee49c499698bb82a6a0cad8ecadf1e4f5b
edrikfsteiner <edrikfsteiner@gmail.com>
2025-08-12 20:19:32 -0300

    Handler and exception corrections

--- api/src/main/java/com/tribalpaw/api/handler/ExceptionDetails.java
+++ api/src/main/java/com/tribalpaw/api/handler/ExceptionDetails.java
@@ -1,11 +1,9 @@
 package com.tribalpaw.api.handler;
 
 import lombok.Builder;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
 @Builder
 public class ExceptionDetails {
     private LocalDateTime timestamp;
--- api/src/main/java/com/tribalpaw/api/handler/RestExceptionHandler.java
+++ api/src/main/java/com/tribalpaw/api/handler/RestExceptionHandler.java
@@ -1,42 +1,16 @@
 package com.tribalpaw.api.handler;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import com.tribalpaw.api.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodNotValid(MethodArgumentNotValidException ex) {
-        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-        String fields = fieldErrors.stream()
-                .map(FieldError::getField)
-                .collect(Collectors.joining(", "));
-        String fieldsMessage = fieldErrors.stream()
-                .map(FieldError::getDefaultMessage)
-                .collect(Collectors.joining(", "));
-
-        return new ResponseEntity<>(
-                ValidationExceptionDetails.builder()
-                        .fields(fields)
-                        .fieldsMessage(fieldsMessage)
-                        .timestamp(LocalDateTime.now())
-                        .status(HttpStatus.METHOD_NOT_ALLOWED.value())
-                        .details(ex.getMessage())
-                        .build(),
-                HttpStatus.METHOD_NOT_ALLOWED
-        );
-    }
-
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<Object> handleInternalServer(InternalServerException ex) {
         return new ResponseEntity<>(
